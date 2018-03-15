afdb807050f71660eb23bd6eb87e09d977c27bf4
Stephen Engler <sengler1989@gmail.com>
2018-03-15 13:45:43 -0500

    set up get and post

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -5,8 +5,11 @@ $(document).ready(readyNow);
 function readyNow(){
     console.log('document ready');
     $('#submitButton').on('click', submitGame);
+    //get the games when page loads
+    getAllGames();
 }
-
+//app.get('/game') and app.post('/game') can be the same because different type of requests
+//url at top of browser can only make a get request.
 function submitGame(){
     let gameName = $('#gameName').val();
     let cost = $('#cost').val();
@@ -15,10 +18,32 @@ function submitGame(){
         type: 'POST',
         data: gameToSend,
         url: '/game'
-    }).done(function(response){
+    }).done(function(response){//need to wait till post request sends back response before doing get 
         //response from a post will just be 200 sucess
         console.log(response);
+        //refresh games list
+        getAllGames();
     }).fail(function(response){
         alert('something went wrong');
     });
+}
+
+function getAllGames(){
+    $.ajax({
+        type: 'GET',
+        url: '/game'
+    }).done(function(response){
+        appendToDom(response);//the response is our gameCollection array
+    });
+}
+
+function appendToDom(gameCollection){
+    $('#gameContent').empty();
+    for(let game of gameCollection){
+        console.log('game ', game);
+        let tr =$('<tr></tr>');
+        tr.append('<td>' + game.name + '</td>');
+        tr.append('<td> '+ game.cost+'</td>');
+        $('#gameContent').append(tr);
+    }
 }
--- server/server.js
+++ server/server.js
@@ -1,13 +1,33 @@
 let express = require('express');
 let app = express();
+let bodyParser = require('body-parser');
+const gameCollection = [];//objects with a name and a cost
 
+//configures body parser for jquery
+app.use(bodyParser.urlencoded({extended:true}));
 const PORT = 4001;
 
 //serve static files
 
 app.use(express.static('server/public'));
 
+//send back all the games
+app.get('/game', (req, res)=>{
+    res.send(gameCollection);
+});
+
+//add a new game
+app.post('/game', (req, res)=>{
+    console.log(req.body);
+    let gameToAdd= req.body;
+    gameCollection.push(gameToAdd);
+    console.log(gameCollection);
+    res.sendStatus(200);
+});
+
+
+
 //spin up server
 app.listen(PORT, ()=>{
     console.log('server is running on ' + PORT);
-})
+});
